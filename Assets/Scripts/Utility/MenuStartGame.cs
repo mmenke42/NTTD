440b82ea45d468c3584be37c5e3b138529b910a5
Gursimar Virdi <385491@colum.edu>
2024-05-01 15:36:56 -0500

    UIChange, LevelDesign, and FinishingUP ThirdLevel

--- Assets/Scripts/Utility/MenuStartGame.cs
+++ Assets/Scripts/Utility/MenuStartGame.cs
@@ -12,7 +12,11 @@ public class MenuStartGame : MonoBehaviour
 
     public void QuitGame()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 
     public void RestartGame()
